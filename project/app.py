7f57bb74f8aacfd880d10fe356cfeb89f1d12ea9
haripoorna <haripoornakumar@gmail.com>
2020-03-10 12:28:08 +0530

    commit

--- project/app.py
+++ project/app.py
@@ -1,26 +1,54 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 ## POST -> recieves data
 ## GET -> sends data
 
-#POST /store data: {name}
+stores = [
+    {
+        'name': 'ganga raju kirana kottu',
+        'items': [
+            {
+                'id': 1,
+                'name': 'santoor soap',
+                'cost': 15
+            }
+        ]
+    }
+]
+
+
+# POST /store data: {name}
 
 @app.route('/store', methods=['POST'])
 def Create_Store():
     pass
 
-@app.route('/store/<string:name>') # 'https://0.0.0.0:5000/store/some_name'
-def get_stores(name):
+
+@app.route('/store/<string:name>')  # 'https://0.0.0.0:5000/store/some_name'
+def get_store(name):
     pass
 
-@app.route('/store') # 'https://0.0.0.0:5000/store/some_name'
-def get_store():
+
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store():
     pass
 
-# @app.route('/')
-# def home():
-#     return 'hello, world'
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store():
+    pass
+
+
+@app.route('/')
+def home():
+    return 'hello, world'
+
 
 app.run(port=1234)
